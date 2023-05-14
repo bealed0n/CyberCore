0a9eae3ea254b4342609b0ecdb8b13a663b0ff9b
bealed0n <benj.loyola@duocuc.cl>
2023-05-13 20:55:58 -0400

    actualizacion bdd

--- cybercore.sql
+++ cybercore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2023 a las 18:17:52
+-- Tiempo de generación: 14-05-2023 a las 02:55:08
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -54,7 +54,9 @@ INSERT INTO `tb_carrito` (`id`, `id_pedido`, `producto`, `detalle`, `cantidad`, 
 (9, '2', 'cables xml', 'ninguno', '3', '6500', '19500', NULL, NULL, NULL, '2023-05-13 11:48:48', NULL, NULL, '1'),
 (10, '3', 'dgdf', 'gdfg', '34', '34', '1156', NULL, NULL, NULL, '2023-05-13 11:56:10', '2023-05-13 11:56:12', '2023-05-13 11:58:02', '0'),
 (11, '3', 'fsdf', 'fsdfsdf', '23', '3233', '74359', NULL, NULL, NULL, '2023-05-13 11:57:45', '2023-05-13 11:57:46', '2023-05-13 11:58:02', '0'),
-(12, '3', 'dsfsdf', 'sdfsdf', '3434', '3434', '11792356', NULL, NULL, NULL, '2023-05-13 11:58:01', NULL, '2023-05-13 11:58:02', '0');
+(12, '3', 'dsfsdf', 'sdfsdf', '3434', '3434', '11792356', NULL, NULL, NULL, '2023-05-13 11:58:01', NULL, '2023-05-13 11:58:02', '0'),
+(13, '3', 'Mesa de sonido', 'pioner', '2', '300990', '601980', NULL, NULL, NULL, '2023-05-13 05:33:21', NULL, NULL, '1'),
+(14, '4', 'guitarra acustica', 'madera de roble', '1', '600000', '600000', NULL, NULL, NULL, '2023-05-13 05:36:45', NULL, NULL, '1');
 
 -- --------------------------------------------------------
 
@@ -68,27 +70,6 @@ CREATE TABLE `tb_delivery` (
   `id_carrito` varchar(512) DEFAULT NULL,
   `id_repartidor_asignado` varchar(512) DEFAULT NULL,
   `estado_delivery` varchar(512) DEFAULT NULL,
-  `user_creacion` varchar(512) DEFAULT NULL,
-  `user_actualizacion` varchar(512) DEFAULT NULL,
-  `user_eliminacion` varchar(512) DEFAULT NULL,
-  `fyh_creacion` datetime DEFAULT NULL,
-  `fyh_actualizacion` datetime DEFAULT NULL,
-  `fyh_eliminacion` datetime DEFAULT NULL,
-  `estado` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tb_mis_direcciones`
---
-
-CREATE TABLE `tb_mis_direcciones` (
-  `id` int(11) NOT NULL,
-  `email` varchar(512) DEFAULT NULL,
-  `nombre_direccion` varchar(512) DEFAULT NULL,
-  `direccion` varchar(512) DEFAULT NULL,
-  `referencia` varchar(512) DEFAULT NULL,
   `user_creacion` varchar(512) DEFAULT NULL,
   `user_actualizacion` varchar(512) DEFAULT NULL,
   `user_eliminacion` varchar(512) DEFAULT NULL,
@@ -134,35 +115,14 @@ CREATE TABLE `tb_pedidos` (
 
 INSERT INTO `tb_pedidos` (`id_pedido`, `nombre_cliente`, `rut_cliente`, `celular_cliente`, `celular_referencia_cliente`, `email_cliente`, `direccion_cliente`, `id_direccion_cliente`, `costo_pedido`, `costo_delivery`, `obs`, `id_carrito`, `id_repartidor_asignado`, `estado_pedido`, `user_creacion`, `user_actualizacion`, `user_eliminacion`, `fyh_creacion`, `fyh_actualizacion`, `fyh_eliminacion`, `estado`) VALUES
 (4, 'prdrito', '1-921', '940327883', '940327883', 'pedrito@gmail.com', 'isla walton 543', '', '99000', '4990', 'objeto delicado', '1', '44', 'PEDIDO FINALIZADO', NULL, NULL, NULL, '2023-05-13 11:14:15', NULL, NULL, '1'),
-(5, 'matias del rio', '494-2', '940327883', '992939323', 'matiaselmismisimo@gmail.com', 'sucasa 123', '', '464490', '7000', 'arbol fuera de casa', '2', '45', 'ASIGNADO', NULL, NULL, NULL, '2023-05-13 11:51:27', NULL, NULL, '1');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tb_repartidor`
---
-
-CREATE TABLE `tb_repartidor` (
-  `id` int(11) NOT NULL,
-  `nombres` varchar(512) DEFAULT NULL,
-  `ap_paterno` varchar(512) DEFAULT NULL,
-  `ap_materno` varchar(512) DEFAULT NULL,
-  `rut` varchar(512) DEFAULT NULL,
-  `fecha_nacimiento` varchar(512) DEFAULT NULL,
-  `sexo` varchar(512) DEFAULT NULL,
-  `celular` varchar(512) DEFAULT NULL,
-  `cargo` varchar(512) DEFAULT NULL,
-  `email` varchar(512) DEFAULT NULL,
-  `password` varchar(512) DEFAULT NULL,
-  `token` varchar(512) DEFAULT NULL,
-  `user_creacion` varchar(512) DEFAULT NULL,
-  `user_actualizacion` varchar(512) DEFAULT NULL,
-  `user_eliminacion` varchar(512) DEFAULT NULL,
-  `fyh_creacion` datetime DEFAULT NULL,
-  `fyh_actualizacion` datetime DEFAULT NULL,
-  `fyh_eliminacion` datetime DEFAULT NULL,
-  `estado` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+(5, 'matias del rio', '494-2', '940327883', '992939323', 'matiaselmismisimo@gmail.com', 'sucasa 123', '', '464490', '7000', 'arbol fuera de casa', '2', '44', 'PEDIDO FINALIZADO', NULL, NULL, NULL, '2023-05-13 11:51:27', NULL, NULL, '1'),
+(6, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(8, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'probando api', '1-9201', '940327883', '940327883', 'benjita@gmail.com', 'calle muy falsa 123', NULL, NULL, '9990', 'que pasopadrino', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(10, 'probando api2', '1-92013', '940327883', '940327883', 'benjita23@gmail.com', 'calle muy falsa 1233', NULL, '40094', '9990', 'que pasopadrino XDDD', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(12, 'german toro', '83344-3', '940327883', '940327883', 'guille@gmail.com', 'isla falsa 123', '', '601980', '4500', 'delicado', '3', '45', 'ASIGNADO', NULL, NULL, NULL, '2023-05-13 05:34:21', NULL, NULL, '1'),
+(13, 'Benjamín', '2323232', '940327883', '940327883', 'benjam@gmail.com', 'Isla adelaida 541', '', '600000', '5000', 'ninguna', '4', '44', 'ASIGNADO', NULL, NULL, NULL, '2023-05-13 05:37:17', NULL, NULL, '1');
 
 -- --------------------------------------------------------
 
@@ -232,10 +192,11 @@ CREATE TABLE `tb_usuarios` (
 --
 
 INSERT INTO `tb_usuarios` (`id`, `nombres`, `ap_paterno`, `ap_materno`, `rut`, `fecha_nacimiento`, `sexo`, `celular`, `foto_perfil`, `email`, `password`, `token`, `cargo`, `user_creacion`, `user_actualizacion`, `user_eliminacion`, `fyh_creacion`, `fyh_actualizacion`, `fyh_eliminacion`, `estado`) VALUES
-(1, 'alumnito', 'elguru', 'alumnito1', '1-9', '2002-06-03', 'masculino', '940327883', NULL, 'benj.loyola@duocuc.cl', '123', NULL, 'ADMINISTRADOR', NULL, NULL, NULL, NULL, NULL, NULL, '1'),
+(1, 'benja', 'elguru', 'alumnito1', '1-9', '2002-06-03', 'MASCULINO', '940327883', NULL, 'benj.loyola@duocuc.cl', '123', NULL, 'ADMINISTRADOR', NULL, NULL, NULL, NULL, NULL, NULL, '1'),
 (41, 'Jose', 'Pepe', 'Grillo', '1-3', '1973-06-03', 'HOMBRE', '965849858', NULL, 'elpepe@gmail.com', '123', NULL, 'ADMINISTRADOR', NULL, NULL, NULL, '2023-05-12 11:07:45', NULL, NULL, '1'),
 (44, 'Bastian', 'Landaeta', 'Loyola', '1212121', '1992-03-09', 'HOMBRE', '940327883', NULL, 'benjaminalexander2002@gmail.com', '123', NULL, 'REPARTIDOR', NULL, NULL, NULL, '2023-05-12 11:47:35', NULL, NULL, '1'),
-(45, 'elpepe', 'profesor', 'decounter', '34949934-2', '1950-04-04', 'MASCULINO', '940327883', NULL, 'elpepe1@gmail.com', '123', NULL, 'REPARTIDOR', NULL, NULL, NULL, '2023-05-13 11:52:16', NULL, NULL, '1');
+(45, 'elpepe', 'profesor', 'decounter', '34949934-2', '1950-04-04', 'MASCULINO', '940327883', NULL, 'elpepe1@gmail.com', '123', NULL, 'REPARTIDOR', NULL, NULL, NULL, '2023-05-13 11:52:16', NULL, NULL, '1'),
+(46, 'Alexander', 'Salazar', 'Loaza', '2100-3', '1973-06-03', 'FEMENINO', '940327883', NULL, 'admin@gmail.com', '123', NULL, 'ADMINISTRADOR', NULL, NULL, NULL, NULL, NULL, NULL, '1');
 
 --
 -- Índices para tablas volcadas
@@ -254,22 +215,10 @@ ALTER TABLE `tb_delivery`
   ADD PRIMARY KEY (`id_delivery`);
 
 --
--- Indices de la tabla `tb_mis_direcciones`
---
-ALTER TABLE `tb_mis_direcciones`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `tb_pedidos`
 --
 ALTER TABLE `tb_pedidos`
   ADD PRIMARY KEY (`id_pedido`);
-
---
--- Indices de la tabla `tb_repartidor`
---
-ALTER TABLE `tb_repartidor`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `tb_ubicacion`
@@ -291,7 +240,7 @@ ALTER TABLE `tb_usuarios`
 -- AUTO_INCREMENT de la tabla `tb_carrito`
 --
 ALTER TABLE `tb_carrito`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_delivery`
@@ -300,22 +249,10 @@ ALTER TABLE `tb_delivery`
   MODIFY `id_delivery` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tb_mis_direcciones`
---
-ALTER TABLE `tb_mis_direcciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de la tabla `tb_pedidos`
 --
 ALTER TABLE `tb_pedidos`
-  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `tb_repartidor`
---
-ALTER TABLE `tb_repartidor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_ubicacion`
@@ -327,7 +264,7 @@ ALTER TABLE `tb_ubicacion`
 -- AUTO_INCREMENT de la tabla `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
