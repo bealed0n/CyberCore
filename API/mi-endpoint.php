7aa0ddd3063a366783060d43f4244b2d9a073a61
bealed0n <benj.loyola@duocuc.cl>
2023-05-21 16:46:30 -0400

    endpoint creado

--- API/mi-endpoint.php
+++ API/mi-endpoint.php
@@ -1,9 +1,13 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/api/v1/mi-endpoint') {
-    // Generar la mensaje 
-    $mensaje = "Hola bien y tu?";
+// Definir el mensaje a devolver en el endpoint
+$mensaje = "Hola, bien y tu?";
 
-    // Enviar el mensaje
-    echo $mensake;
-}
-?>
+// Establecer las cabeceras para permitir el acceso desde otros dominios
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Crear el array de respuesta
+$respuesta = array("mensaje" => $mensaje);
+
+// Devolver la respuesta en formato JSON
+echo json_encode($respuesta);
--- app/config/config.php
+++ app/config/config.php
@@ -17,3 +17,18 @@ try{
 }catch (PDOException $e){
     echo "<script>alert('Error a la conexión con la base de datos')</script>";
 }
+
+$requestUrl = $_SERVER['REQUEST_URI'];
+
+switch ($requestUrl) {
+    case '/api/v1/mi-endpoint':
+        // Lógica para manejar la solicitud del endpoint '/api/v1/mi-endpoint'
+        echo 'Hola bien y tú';
+        break;
+    // Agrega más casos para manejar otras rutas aquí
+    default:
+        // Ruta no encontrada
+        http_response_code(404);
+        echo '404 - Ruta no encontrada';
+        break;
+}
