6660f435702364c6164cc09e376a1826a053eeeb
bealed0n <benj.loyola@duocuc.cl>
2023-06-18 20:19:12 -0400

    actualizacion seguimiento

--- API/codigo_seguimientoAPI.php
+++ API/codigo_seguimientoAPI.php
@@ -6,19 +6,46 @@ include "utils.php";
 $dbConn = connect($db);
 
 /*
-  Obtener estado de un pedido específico
+  Obtener estado y número de seguimiento de un pedido específico
  */
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['codigo_seguimiento'])) {
-    $codigoSeguimiento = $_GET['codigo_seguimiento'];
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['codigo_seguimiento'])) {
+        $codigoSeguimiento = $_GET['codigo_seguimiento'];
 
-    $sql = $dbConn->prepare("SELECT estado FROM estado_pedidos WHERE codigo_seguimiento = :codigo_seguimiento");
-    $sql->bindParam(':codigo_seguimiento', $codigoSeguimiento);
-    $sql->execute();
-    $result = $sql->fetch(PDO::FETCH_ASSOC);
+        $sql = $dbConn->prepare("SELECT estado FROM estado_pedidos WHERE codigo_seguimiento = :codigo_seguimiento");
+        $sql->bindParam(':codigo_seguimiento', $codigoSeguimiento);
+        $sql->execute();
+        $result = $sql->fetch(PDO::FETCH_ASSOC);
 
-    header("Content-Type: application/json");
-    echo json_encode($result);
-    exit();
+        if ($result) {
+            header("Content-Type: application/json");
+            echo json_encode($result);
+            exit();
+        } else {
+            // Si no se encuentra un estado con el código de seguimiento proporcionado
+            header("HTTP/1.1 404 Not Found");
+            echo json_encode(array('error' => 'Estado no encontrado'));
+            exit();
+        }
+    } elseif (isset($_GET['pedido_id'])) {
+        $pedidoId = $_GET['pedido_id'];
+
+        $sql = $dbConn->prepare("SELECT codigo_seguimiento, estado FROM estado_pedidos WHERE pedido_id = :pedido_id");
+        $sql->bindParam(':pedido_id', $pedidoId);
+        $sql->execute();
+        $result = $sql->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            header("Content-Type: application/json");
+            echo json_encode($result);
+            exit();
+        } else {
+            // Si no se encuentra un registro con el pedido_id proporcionado
+            header("HTTP/1.1 404 Not Found");
+            echo json_encode(array('error' => 'Pedido no encontrado'));
+            exit();
+        }
+    }
 }
 
 // En caso de que ninguna opción válida se haya ejecutado
